b4f0ecef61409c635531be2c2c501e3d93c0b60d
sastra12 <anonymousonly594@gmail.com>
2022-07-15 14:52:05 +0700

    make helpers

---
+++ app/Http/Helpers/helpers.php
@@ -0,0 +1,54 @@
+<!-- Make helpers -->
+<!-- Register to composer.json(autoload)
+so that it can be accessed in all 
+application components. For testing helpers 
+run composer dump-autoload -->
+
+<?php
+function format_uang($angka)
+{
+    return number_format($angka, 0, ',', '.');
+}
+
+function terbilang($angka)
+{
+    $angka = abs($angka);
+    $baca = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+    $terbilang = '';
+
+    if ($angka < 12) {
+        $terbilang = '' . $baca[$angka];
+    } elseif ($angka < 20) {
+        $terbilang = terbilang($angka - 10) . ' belas';
+    } elseif ($angka < 100) {
+        $terbilang = terbilang($angka / 10) . ' puluh ' . terbilang($angka % 10);
+    } elseif ($angka < 200) {
+        $terbilang = 'seratus ' . terbilang($angka - 100);
+    } elseif ($angka < 1000) {
+        $terbilang = terbilang($angka / 100) . ' ratus ' . terbilang($angka % 100);
+    } elseif ($angka < 2000) {
+        $terbilang = 'seribu' . terbilang($angka - 1000);
+    } elseif ($angka < 1000000) {
+        $terbilang = terbilang($angka / 1000) . ' ribu ' . terbilang($angka % 1000);
+    } elseif ($angka < 1000000000) {
+        $terbilang = terbilang($angka / 1000000) . ' juta ' . terbilang($angka % 1000000);
+    }
+
+    return $terbilang;
+}
+
+//2021-03(bulan)-06
+function format_tanggal($tgl)
+{
+    $nama_bulan = [
+        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    ];
+    $text = '';
+    $tahun = substr($tgl, 0, 4);
+    $bulan = $nama_bulan[(int) substr($tgl, 5, 2) - 1];
+    $tanggal = substr($tgl, 8, 2);
+    $text .= '' . $tanggal . ' ' . $bulan . ' ' . $tahun;
+    return $text;
+}
+
+?>
