8188f6d54ad08d08b264a5f32c5521a1c0b3a4d9
sastra12 <anonymousonly594@gmail.com>
2022-08-03 12:19:27 +0700

    edit view login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -24,31 +24,25 @@
     <div class="login-box">
         <div class="card">
             <div class="card-body login-card-body">
+
                 <form action="{{ route('authenticate') }}" method="post">
                     @csrf
-                    <div class="input-group mb-3">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email</label>
                         <input type="email" class="form-control @error('title') is-invalid @enderror"
-                            placeholder="Email" name="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
+                            id="exampleInputEmail1" name="email">
                         @error('email')
-                            <div class="alert alert-danger">{{ $message }}</div>
+                            <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                         @error('password')
-                            <div class="alert alert-danger">{{ $message }}</div>
+                            <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
+
                     <div class="row">
                         <!-- /.col -->
                         <div class="col-md">
