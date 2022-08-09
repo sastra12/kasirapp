c30e13a7c20fdc77edceded619cdce39fb760aa7
sastra12 <anonymousonly594@gmail.com>
2022-08-09 09:39:41 +0700

    fixed add kategori

---
+++ resources/views/kategori/form.blade.php
@@ -0,0 +1,33 @@
+<!-- Modal -->
+<div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <ul id="error_list">
+
+                </ul>
+                <form action="" method="post">
+                    @csrf
+                    @method('post')
+                    <div class="form-group">
+                        <label for="kategori">Kategori</label>
+                        <input type="text" class="form-control" id="kategori" name="nama_kategori"
+                            value="{{ old('kategori') }}">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</div>
